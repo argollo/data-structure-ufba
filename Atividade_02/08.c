b9abe842715cfd70ef8e07d3fa5dcbdc6988da59
André Argôlo <felipeargolo@obomdoacupe.com>
2015-03-24 17:00:43 -0300

    questão 8 atualizada

--- Atividade_02/08.c
+++ Atividade_02/08.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
    
 int main(void){
-int a, b, c;
-scanf("%d %d %d", &a, &b, &c);
-          
+int opcao, valor1, valor2;
+printf("1. Somar dois números.\n2. Subtrair dois números\n3. Multiplicar dois números\n4. Dividir dois números\n5. Raiz quadrada de um número\nDigite a opção desejada:");
+scanf("%d %d %d", &opcao, &valor1, &valor2);
 return 0;
 }
-
-
